45e62a5efe40be20ab651c727f732e43d936eb7a
julika3 <105982976+julika3@users.noreply.github.com>
2022-12-29 14:33:34 +0100

    legend for terminal type

--- visualisation.py
+++ visualisation.py
@@ -7,6 +7,7 @@ df_map = pd.read_excel('database.xlsx', skiprows=0)
 df_map['annual capacity'] = df_map['annual capacity'].map(lambda x: 0 if x == '?' else float(x))
 df_map['latitude'] = df_map['latitude'].map(lambda x: float(x))
 df_map['longitude'] = df_map['longitude'].map(lambda x: float(x))
+# description to be displayed in as hover text (html formatting)
 df_map['description'] = 'Location: ' + df_map['location'] \
                         + '<br>Type: ' + df_map['type'] \
                         + '<br>Start up Date: ' + df_map['start up date'].astype(str) \
@@ -18,6 +19,10 @@ def create_map(df):
     fig = go.Figure()
 
     fig.update_layout(
+        title='LNG Terminals in Europe',
+        title_x=0.5,
+        showlegend=True,
+        legend_title='Facility Type',
         geo=go.layout.Geo(
             resolution=50,
             scope='europe',
@@ -31,8 +36,6 @@ def create_map(df):
             lonaxis_range=[df['longitude'].min() - 5, df['longitude'].max() + 5],
             lataxis_range=[df['latitude'].min() - 5, df['latitude'].max() + 5],
         ),
-
-        legend_traceorder='reversed'
     )
 
     # bubble_sizes = df['annual capacity'] / df['annual capacity'].max()
@@ -43,20 +46,24 @@ def create_map(df):
 
 
     # add the terminals to the map via their latitude and longitude coordinates
-    fig.add_trace(go.Scattergeo(
-        lon=df['longitude'],
-        lat=df['latitude'],
-        text=df['description'],
-        marker=dict(
-            # scale the bubbles according to their relative annual capacity
-            size=df['annual capacity'] / df['annual capacity'].max() * 200,
-            # colour bubbles according to set dictionary
-            color=df['type'].map(type_clr_dct),
-            line_color='black',
-            line_width=0.5,
-            sizemode='area'
-        )
-    ))
+    for key in type_clr_dct.keys():
+        df_plot = df[df['type'] == key]
+        fig.add_trace(go.Scattergeo(
+            name=key,
+            lon=df_plot['longitude'],
+            lat=df_plot['latitude'],
+            text=df_plot['description'],
+            marker=dict(
+                # scale the bubbles according to their relative annual capacity
+                size=df_plot['annual capacity'] / df_plot['annual capacity'].max() * 200,
+                # colour bubbles according to set dictionary
+                color=df_plot['type'].map(type_clr_dct),
+                opacity=0.6,
+                line_color='black',
+                line_width=0.6,
+                sizemode='area'
+            )
+        ))
 
     fig.update_layout(
         height=1200,
